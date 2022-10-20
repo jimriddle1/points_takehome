daea31310ac5c4fcd717e783a14b5c49c70476ff
Jim Riddle <jimriddle0@gmail.com>
2022-10-20 16:07:51 -0500

    wrote model method for spending points, giving total points

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,15 +4,37 @@ class Transaction < ApplicationRecord
   validates :timestamp, presence: true
 
   def self.points_balance
-    output_hash = {}
-    Transaction.all.each do |transaction|
-      if output_hash.keys.include?(transaction[:payer])
-        output_hash[transaction[:payer]] += transaction[:points]
+    Transaction.group(:payer).sum(:points)
+  end
+
+  def self.spend_points(amount)
+    output_array = []
+    original_balance = points_balance
+    transactions_in_order = Transaction.order(:timestamp)
+
+    transactions_in_order.each do |transaction|
+      if transaction.points < amount
+        amount -= transaction.points
+        transaction.update(points: 0)
       else
-        output_hash[transaction[:payer]] = transaction[:points]
+        balance = transaction.points - amount
+        transaction.update(points: balance)
+        amount = 0
+        break
       end
     end
-    return output_hash
+
+    points_balance.each do |transaction|
+      amount = transaction[1] - original_balance[transaction[0]]
+      output_array << {"payer": transaction[0], "points": amount}
+    end
+
+    return output_array
+
+  end
+
+  def self.total_points_available
+     Transaction.sum(:points)
   end
 
 end
