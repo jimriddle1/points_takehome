b3952d7358112b9435e8cdf49b77b2519464a4a1
Jim Riddle <jimriddle0@gmail.com>
2022-10-20 13:39:56 -0500

    modified controller, included serializer to create transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,8 +1,10 @@
 class Transaction < ApplicationRecord
-
+  validates :payer, presence: true
+  validates :points, presence: true
+  validates :timestamp, presence: true
 
   def self.points_balance
-    binding.pry
+    # binding.pry
   end
 
 end
