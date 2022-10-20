a84b23ff23be0f779e2fb8beb204773da03fe002
Jim Riddle <jimriddle0@gmail.com>
2022-10-20 13:39:42 -0500

    wrote test for creating a transaction, including a sad path

--- spec/api/v1/transactions_request_spec.rb
+++ spec/api/v1/transactions_request_spec.rb
@@ -14,10 +14,26 @@ RSpec.describe 'Transactions API' do
      expect(response).to be_successful
 
      created_transaction = Transaction.last
+
      expect(created_transaction).to be_a(Transaction)
      expect(created_transaction.payer).to be_a(String)
      expect(created_transaction.points).to be_a(Integer)
      expect(created_transaction.timestamp).to be_a(String)
+  end
+
+  it 'can create a new transaction - sad path' do
+
+    transaction_params = {
+      payer: 'DANNON'
+      }
+
+     post '/api/v1/transactions', params: { transaction: transaction_params }, as: :json
+     expect(response).to_not be_successful
+     response_body = JSON.parse(response.body, symbolize_names: true)
+
+     expect(response_body[:data][:errors][0]).to eq("Points can't be blank")
+     expect(response_body[:data][:errors][1]).to eq("Timestamp can't be blank")
+
   end
 
 end
